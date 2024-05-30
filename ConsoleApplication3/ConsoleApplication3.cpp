84c3b232f2d2d6576cbdf75885d2e0d5f553fce2
mdeandwibekti <mdeandwibekti@gmail.com>
2024-05-30 17:42:33 +0700

    else diposisi terakhir array

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -28,7 +28,17 @@ public:
 			FRONT = 0;
 			REAR = 0;
 		}
+		else {
+			//jika REAR berada diposisi terakhir array,kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+		}
+		queue_array[REAR] = num;
 	}
+
+
 
 };
 
