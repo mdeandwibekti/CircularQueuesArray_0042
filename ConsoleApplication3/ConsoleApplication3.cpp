939c4a3dbbf04165f4df535137d2fe9b4e4c505e
mdeandwibekti <mdeandwibekti@gmail.com>
2024-05-30 18:35:03 +0700

    if while iterasi dri front dan rear

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -72,7 +72,27 @@ public:
 
 		cout << "\nelements in the queue are ...\n";
 
-		//jikan FRON
+		//jikan FRONT _position <= REAR_position,iterasi dari FRONT hingga REAR 
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "   "; 
+				FRONT_position++;
+			}
+			cout << endl;
+		}
+		else {
+			while (FRONT_position <= max - 1) {
+				cout << queue_array[FRONT_position] << "   ";
+				FRONT_position++;
+			}
+
+			FRONT_position = 0;
+
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "   ";
+			}
+			cout << endl;
+		}
 	}
 };
 
