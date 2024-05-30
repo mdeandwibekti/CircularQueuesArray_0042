02c324f9f6aa8ad6f9c9146aea17d4a92fe2f99a
mdeandwibekti <mdeandwibekti@gmail.com>
2024-05-30 17:38:25 +0700

    public void inssert

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -5,3 +5,24 @@ class queues {
 	int FRONT, REAR, max = 5;
 	int queue_array[5];
 
+public:
+	queues() {
+		FRONT = -1;
+		REAR = -1;
+	}
+
+	void insert() {
+		int num;
+		cout << "enter a number: ";
+		cin >> num;
+		cout << endl;
+
+		//cek apakah antian penuh
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nqueue overflow\n";
+
+		}
+	}
+
+};
+
